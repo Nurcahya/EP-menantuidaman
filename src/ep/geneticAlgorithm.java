433c9a82396999ed72325c79fda94084c1a9478f
Nurcahya Pradana Taufik Prakisya <endlesslightz@yahoo.com>
2016-03-29 11:49:03 +0700

    Update geneticAlgorithm.java

--- src/ep/geneticAlgorithm.java
+++ src/ep/geneticAlgorithm.java
@@ -9,33 +9,33 @@ import java.util.Random;
 public class geneticAlgorithm {
     int jumMutasi;
     public geneticAlgorithm(String filename, int mutasi) throws FileNotFoundException {
-        mate(growParents(filename), mutasi);
+        mate(individu(filename), mutasi);
     }
 
     private void mate(PriorityQueue<evolutionaryDecisionTree> family, int mutasi) {
-        evolutionaryDecisionTree mom = family.poll();
-        evolutionaryDecisionTree dad = family.poll();
+        evolutionaryDecisionTree kromosom1 = family.poll();
+        evolutionaryDecisionTree kromosom2 = family.poll();
 
         /** Genetic : */
         Random random = new Random();
         jumMutasi = mutasi;
         int shearLocation = random.nextInt(this.jumMutasi);
-        ArrayList<Node> branch = mom.train().get(shearLocation);
-        dad.train().add(branch);
-        mom.train().remove(branch);
+        ArrayList<Node> branch = kromosom1.train().get(shearLocation);
+        kromosom2.train().add(branch);
+        kromosom1.train().remove(branch);
     }
 
-    private PriorityQueue<evolutionaryDecisionTree> growParents(String filename) throws FileNotFoundException {
+    private PriorityQueue<evolutionaryDecisionTree> individu(String filename) throws FileNotFoundException {
         PriorityQueue<evolutionaryDecisionTree> family = new PriorityQueue<evolutionaryDecisionTree>(4, new Comparator<evolutionaryDecisionTree>() {
             @Override
-            public int compare(evolutionaryDecisionTree mom, evolutionaryDecisionTree dad) throws NullPointerException, ClassCastException {
-                double accuracyMom = test(mom.train(), mom.test());
-                double accuracyDad = test(dad.train(), dad.test());
-                double sizeMom = mom.train().size();
-                double sizeDad = dad.train().size();
-                if ((accuracyMom - sizeMom) < (accuracyDad - sizeDad)) {
+            public int compare(evolutionaryDecisionTree kromosom1, evolutionaryDecisionTree kromosom2) throws NullPointerException, ClassCastException {
+                double accuracy1 = test(kromosom1.train(), kromosom1.test());
+                double accuracy2 = test(kromosom2.train(), kromosom2.test());
+                double size1 = kromosom1.train().size();
+                double size2 = kromosom2.train().size();
+                if ((accuracy1 - size1) < (accuracy2 - size2)) {
                     return -1;
-                } else if ((accuracyMom - sizeMom) == (accuracyDad - sizeDad)) {
+                } else if ((accuracy1 - size2) == (accuracy2 - size2)) {
                     return 0;
                 } else {
                     return 1;
